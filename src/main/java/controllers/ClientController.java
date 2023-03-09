7236d1f9e4ef2747abfad3c575eaed7ff61a5bb5
Tymoteusz Pilarz <tymoteuszpilarz@gmail.com>
2023-03-09 14:37:47 +0100

    Added server, client

--- src/main/java/controllers/ClientController.java
+++ src/main/java/controllers/ClientController.java
@@ -1,5 +1,16 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ClientController
 {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick()
+    {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
 }
